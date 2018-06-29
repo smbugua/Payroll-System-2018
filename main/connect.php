656611d3c47f027892a1e2c78d84c1a3b810aa05
simon mbugua <smbugua11@gmail.com>
2018-06-29 12:55:59 +0300

    ok

--- main/connect.php
+++ main/connect.php
@@ -60,4 +60,12 @@ function longdate($timestamp)
 {
 return date("l F jS Y", $timestamp);
 }
+function getTaxableIncome($staffid,$nssf){
+	$r=mysql_fetch_array(mysql_query("SELECT salary,totalbenefits FROM payroll_tbl where staffid='$id'"));
+	$bens=$r['totalbenefits'];
+	$salary=$r['salary'];
+	$taxableincome=($bens+$salary)-$nssf;
+	return $taxableincome;
+
+}
 ?>
